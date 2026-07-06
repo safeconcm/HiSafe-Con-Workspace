3c0a05c00083ad1931d388bf433c8edb64ca074b
safeconcm <safeconcm@gmail.com>
2026-07-06 21:58:13 +0700

    Create route.ts

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,12 @@
+// src/app/api/auth/logout/route.ts
+import { createServerSupabaseClient } from '@/lib/supabase/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+  const supabase = await createServerSupabaseClient()
+  await supabase.auth.signOut()
+
+  const response = NextResponse.redirect(new URL('/login', req.url))
+  response.cookies.delete('hsc_session')
+  return response
+}
